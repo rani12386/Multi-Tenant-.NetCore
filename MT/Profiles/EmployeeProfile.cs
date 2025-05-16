f246cb5c784166bdf48a4924d57de047ab95e4b1
MDLDOM01\gangir <rani.gangi@tvsscs.com>
2025-05-16 19:52:32 +0100

    Added profiles for auto mapping

---
+++ MT/Profiles/EmployeeProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MT.Models;
+using MT_Data.Entities;
+
+namespace MT.Profiles;
+public class EmployeeProfile : Profile
+{
+    public EmployeeProfile()
+    {
+        CreateMap<EmployeeViewModel, Employee>()
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
+        //.ForPath(dest => dest.DepartmentName,
+        //   opt => opt.MapFrom(src => src.Department.Name)); //here the dest is represents viewmodel and the src represents entity.
+    }
+}
